059e7c448a6e2a7e0e6ee17f203b2dae8f2b7629
arhh <aulee7@icloud.com>
2020-07-29 20:23:25 +0100

    Add test for CSVToAPI file

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Test Java Main method... Done");
+        String CSVFilename = args[0];
+    }
+
+    private static boolean testCSVToAPI(String csvFile) {
+        boolean testSuccess = true;
+        CSVToAPI csvAPIInterpreter;
+        boolean returnStatus;
+        System.out.println("Beginning test of CSVToAPI class...");
+
+        // Pass a CSV file to CSVToAPI object constructor
+        csvAPIInterpreter = CSVToAPI.createInterpreter(csvFile);
+
+        // Instruct CSVToAPI object to translate CSV into API calls
+        returnStatus = csvAPIInterpreter.translateCSVToAPI();
+        // Check if successful. Failed transaltion = False return value
+
+        // Instruct CSVToAPI object to make API calls.
+        returnStatus = csvAPIInterpreter.makeAPIRequests();
+        // Check if successful. Failed transaltion = False return value
+
+        System.out.println("Finishing CSVToAPI test...");
+
+        return testSuccess;
     }
 }
